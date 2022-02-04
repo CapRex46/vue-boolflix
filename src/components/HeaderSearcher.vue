8310b1cc46cc6675c6c8732f6700d0f5fd3e9a05
agres <ale.agresta46@gmail.com>
2022-02-04 15:15:32 +0100

    fix

--- src/components/HeaderSearcher.vue
+++ src/components/HeaderSearcher.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="searchnav">
         <div class="navcontent">
-            <h1>
+            <h1 class="titlearea">
                 BOOLFLIX
             </h1>
-            <div>
-                <label>Search title:  </label>
+            <div class="searcharea">
                 <input type="text" v-model="query"  @keyup='$emit("query",getResult)'  placeholder="Search here.."/>
             </div>
         </div>
@@ -27,19 +26,22 @@ export default {
     .searchnav {
         width: 100%;
         background: black;
+        height: 80px;
         .navcontent {
             width: 1400px;
             margin: 0 auto;
             color: rgb(199, 59, 59);
             display: flex;
             justify-content: space-between;
-            .results {
-                width: 1400px;
-                display: flex;
-                flex-wrap: wrap;
-                margin: 0 auto ;
-
-            }
+            padding-top: 20px;
+            input[type=text] {
+                padding: 2px 20px;
+                margin: 8px 0;
+                box-sizing: border-box;
+                border-radius: 4px;
+                 background-color: rgb(77, 75, 75);
+                 color: whitesmoke;
+                }
         }
     }
 </style>
--- src/components/MoviesBox.vue
+++ src/components/MoviesBox.vue
@@ -1,11 +1,16 @@
 <template>
 <main>
         <div class="moviecontainer">
-        <div class="moviestitles" v-for='result in results' :key='result.id'>
-                <p>{{result.title}}</p>
-                <p>{{result.original_title}}</p>
+        <div class="moviestitles">
+            <div class="moviesinfos" v-for='result in results' :key='result.id'>
                 <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
+                <div class="moviestxt">
+                    <p>{{result.title}}</p>
+                    <p>{{result.original_title}}</p>
+                </div>
+            </div>
         </div>
+
     </div>
 </main>
 
@@ -29,15 +34,27 @@ export default {
         width: 100%;
         height: 900px;
         background: rgb(77, 75, 75);
-        .moviestitles {
-            width: 1400px;
-            height: 600px;
-            margin: 0 auto;
-            .moviesimage {
+        display: flex;
+        flex-wrap: wrap;
+            .moviesinfos {
                 width: 200px;
                 height: 300px;
-                border: 1px solid white;
-            }
-        } 
+                color: whitesmoke;
+                padding: 30px;
+                img {
+                   width: 200px;
+                   transition: .5s; 
+                   &:hover{
+                       box-shadow: 0 0 35px rgba(218, 0, 0, 0.2);
+                       cursor: pointer;
+                       transition: .5s;
+                       width: 250px;
+                       .moviestxt p {
+                           text-align: center;
+                       }
+                   } 
+                }
+            } 
+        
     }
 </style>
